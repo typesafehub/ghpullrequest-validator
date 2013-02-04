1513c7307f7e06f12a83347d24f71fee2f3401ad
Adriaan Moors <adriaan.moors@typesafe.com>
2013-02-03 23:39:21 -0800

    better logging

--- src/main/scala/backend/PullRequestChecker.scala
+++ src/main/scala/backend/PullRequestChecker.scala
@@ -78,7 +78,11 @@ class PullRequestChecker(ghapi: GithubAPI, jobBuilderProps: Props) extends Actor
     }
 
     def buildCommit(sha: String, job: JenkinsJob, force: Boolean = false, noop: Boolean = false) = if ( (force && !forced(sha, job)) || !active(sha, job)) {
-      log.debug("Build commit "+ sha +" for #"+ pull.number +" job: "+ job)
+      if (noop)
+        log.debug("Looking for build of "+ sha +" in #"+ pull.number +" job: "+ job)
+      else
+        log.debug("May build commit "+ sha +" for #"+ pull.number +" job: "+ job)
+
       active.+=((sha, job))
       forced.+=((sha, job))
       val forcedUniq = if (force) "-forced" else ""
