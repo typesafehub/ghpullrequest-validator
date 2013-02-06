c94b411560a765a0aef614aa2a9d6877634ef3bc
Adriaan Moors <adriaan.moors@typesafe.com>
2013-02-05 19:56:02 -0800

    simplify build start logic: discover queued builds
    the jenkins REST API is a piece of shit, but we
    can now at least use its queue, which massively
    simplifies the logic in the build start watcher
    
    it's also a little more robust regarding kitten-reboots

--- src/main/scala/backend/JenkinsJobStartWatcher.scala
+++ src/main/scala/backend/JenkinsJobStartWatcher.scala
@@ -10,99 +10,63 @@ import rest.jenkins.BuildStatus
  */
 // TODO - We need a new way to detect a job has started...
 class JenkinsJobStartWatcher(api: JenkinsAPI, b: BuildCommit, jenkinsService: ActorRef) extends Actor with ActorLogging {
-  // TODO - Use better time library....
-
   // Set timeout to check when the job has started.
-  context setReceiveTimeout (30 seconds)
+  context setReceiveTimeout (1 minute)
 
-  private[this] var retryCount = 0
-  private val MAX_RETRIES = 10
-
-  sealed abstract class JenkinsStatus
-  case object JQueue extends JenkinsStatus
-  case object JRetry extends JenkinsStatus
-  case object JStop extends JenkinsStatus
-  def jenkinsStatus: JenkinsStatus =
-    api.jobInfo(b.job).queueItem match {
-      // there's a queue, be patient as jenkins doesn't tell us about queued jobs
-      case Some(rest.jenkins.QueueItem(true)) =>
-        log.debug("Queue " + b.job + " #" + b.args("pullrequest") + " commit " + b.args("sha"))
-        JQueue
-      // no queue, if don't start finding it by now, something went wrong
-      case _ =>
-        log.debug("No queue " + b.job + " #" + b.args("pullrequest") + " commit " + b.args("sha"))
-        if (retryCount < MAX_RETRIES) JRetry else JStop
-    }
-
-  private val knownRunning = collection.mutable.HashSet[String]()
-  private var notifiedCommenter = false // did we add a dummy BuildQueued?
+  private val seenBuilds = collection.mutable.HashSet[String]()
   // we only try to start a build once, if that fails, we'll time out and try again
   private var startedBuild = false
 
   def receive: Receive = {
     case ReceiveTimeout =>
-      jenkinsStatus match {
-        case JStop =>
-          log.debug("Timed out finding build " + (b.job, b.args.get("pullrequest"), api.jobInfo(b.job)))
-          jenkinsService ! b // tell the service to rebuild
-          context stop self // stop looking for the job to start
+      // jobs for this PR, sha, mergebranch (all params must match)
+      val ourJobs = api.buildStatusForJob(b.job).filter { status =>
+        val paramsForOurArgs = status.actions.parameters.collect {
+          case rest.jenkins.Param(n, v) if b.args.isDefinedAt(n) => (n, v)
+        }.toMap
 
-        case s @ (JQueue | JRetry) =>
-          // be patient when there's a queue, don't even start a job as we won't be able to tell whether it started
-          // if we fire off a build, don't realize it started, and reboot the kitten, it will start a redundant job next time,
-          // if the originally started job is still in the queue (as the job is hidden there)
-          if (s == JQueue) context setReceiveTimeout (10 minutes)
-          else {
-            retryCount += 1
-            context setReceiveTimeout (1 minutes)
-          }
+        paramsForOurArgs == b.args
+      }
 
-          // we haven't started our own build yet, but maybe an older one is running
-          // watch its result, but still start our own
-          val relevantBuilds = ourJobs.filter(_.building) match {
-            case bs if bs.isEmpty =>
-              log.debug("No building jobs for " + b.job + " #" + b.args("pullrequest") + " commit " + b.args("sha") + " all jobs: " + ourJobs)
-              ourJobs.headOption.toSet // if no running builds, just look at the most recent one
-            case bs => bs.toSet
-          }
+      // where's our build? if there are building jobs, look at those
+      // if not, shift our gaze to queued or finished jobs
+      val relevantBuilds = ourJobs.filter(_.building) match {
+        case bs if bs.isEmpty =>
+          log.debug("No building jobs for " + b.job + " #" + b.args("pullrequest") + " commit " + b.args("sha") + " all jobs: " + ourJobs)
+          ourJobs.headOption.toSet // if no running builds, just look at the most recent one
+        case bs => bs.toSet
+      }
 
-          val newlyDiscovered = relevantBuilds.filterNot(bs => knownRunning(bs.url))
+      val newlyDiscovered = relevantBuilds.filterNot(bs => seenBuilds(bs.url))
 
-          val (building, done) = newlyDiscovered.partition(_.building)
-          building foreach { status =>
-            jenkinsService ! JobStarted(b, status)
-            b.commenter ! BuildStarted(status.url)
-          }
+      val (buildingOrQueued, done) = newlyDiscovered.partition(st => st.building || st.queued)
 
-          // done after sending BuildStarted so that (hopefully) this status is more recent
-          done foreach { status =>
-            b.commenter ! BuildResult(status)
-          }
+      buildingOrQueued foreach { status =>
+        if (status.queued)
+          b.commenter ! BuildQueued
+        else {
+          jenkinsService ! JobStarted(b, status)
+          b.commenter ! BuildStarted(status.url)
+        }
+      }
 
-          knownRunning ++= newlyDiscovered.map(_.url)
+      // done after sending BuildStarted so that (hopefully) this status is more recent
+      done foreach { status =>
+        b.commenter ! BuildResult(status)
+      }
 
-          val canStop = startedBuild || !b.force
-          val foundJob = newlyDiscovered.nonEmpty
-          if (canStop && foundJob) context stop self
-          else if (s != JQueue && !b.noop && !startedBuild) {
-            startedBuild = true
-            api.buildJob(b.job, b.args)
-            log.debug("Started job for #" + b.args("pullrequest") + " --> " + b.job.name + " sha: " + b.args("sha"))
-          }
+      seenBuilds ++= newlyDiscovered.map(_.url)
 
-          // send BuildQueued no more than once
-          if (!notifiedCommenter) {
-            notifiedCommenter = true // either because newlyDiscovered.nonEmpty and thus we notified above, or we will now
-            if (newlyDiscovered.isEmpty)
-              b.commenter ! BuildQueued
-          }
+      val canStop = startedBuild || !b.force
+      def foundRunningJob = newlyDiscovered.exists(!_.queued)
+      def isQueued = newlyDiscovered.exists(_.queued)
 
+      if (canStop && foundRunningJob) context stop self
+      else if (!b.noop && !startedBuild && !isQueued) {
+        startedBuild = true
+        api.buildJob(b.job, b.args)
+        log.debug("Started job for #" + b.args("pullrequest") + " --> " + b.job.name + " sha: " + b.args("sha"))
       }
   }
-
-  def ourJobs =
-    api.buildStatusForJob(b.job).filter { status =>
-      status.actions.parameters.collect { case rest.jenkins.Param(n, v) if b.args.isDefinedAt(n) => (n, v) }.toMap == b.args
-    }
 
 }
--- src/main/scala/rest/jenkins/api.scala
+++ src/main/scala/rest/jenkins/api.scala
@@ -1,98 +1,88 @@
 package rest
 package jenkins
 
-import dispatch.{Http => _, _}
+import dispatch.{ Http => _, _ }
 import net.liftweb.json.{ DefaultFormats, Formats }
 import net.liftweb.json.JsonParser._
 import backend.JenkinsJob
 
-/**
- * An API class to connect to jenkins and do stuff.
+/** An API class to connect to jenkins and do stuff.
  */
-class API(jenkinsUrl: String, auth: Option[(String,String)] = None) {
+class API(jenkinsUrl: String, auth: Option[(String, String)] = None) {
 
   protected def makeReq(uri: String) = {
     val url = dispatch.url("%s/%s" format (jenkinsUrl, uri))
     auth match {
-      case Some((user,pw)) => url.as_!(user, pw)
-      case _               => url
+      case Some((user, pw)) => url.as_!(user, pw)
+      case _                => url
     }
   }
-  
-  def jobInfo(job: JenkinsJob): Job = {
-    val loc = makeReq("job/%s/api/json" format (job.name))
-    Http(loc >- parseJsonTo[Job])
-  }
-  
-  def buildJob(job: JenkinsJob, params: Map[String,String] = Map.empty): String = {
-    val action = 
-      if (params.isEmpty) makeReq("%s/build" format ( job.name))
+
+  def buildJob(job: JenkinsJob, params: Map[String, String] = Map.empty): String = {
+    val action =
+      if (params.isEmpty) makeReq("%s/build" format (job.name))
       else {
         val uri = makeReq("job/%s/buildWithParameters" format (job.name))
         uri.POST << params
       }
     Http(action >- identity[String])
   }
-  
+
   def buildStatus(job: JenkinsJob, buildNumber: String): BuildStatus = {
     val loc = makeReq("job/%s/%s/api/json" format (job.name, buildNumber))
     Http(loc >- parseJsonTo[BuildStatus])
   }
-  
+
   /** A traversable that lazily pulls build status information from jenkins. */
   def buildStatusForJob(job: JenkinsJob): Stream[BuildStatus] = {
+    def jobInfo(job: JenkinsJob): Job = {
+      val loc = makeReq("job/%s/api/json" format (job.name))
+      Http(loc >- parseJsonTo[Job])
+    }
     val info = jobInfo(job)
     val reportedBuilds = info.builds.sorted
+
+    // hack: retrieve queued jobs from queue/api/json
+    val queuedStati = Http(makeReq("queue/api/json") >- parseJsonTo[Queue]).items.filter(_.jobName == job.name).map(_.toStatus)
 
     // work around https://issues.jenkins-ci.org/browse/JENKINS-15583 -- jenkins not reporting all running builds
     // so hack it by closing the range from the last reported build to the lastBuild in the Json response, which is correct
     // only the builds part of the reply is wrong
-
     val start =
       try {
         if (reportedBuilds.isEmpty) info.firstBuild.number.toInt
         else reportedBuilds.last.number.toInt
       } catch { case x: Exception => 1 }
-    val allBuilds = {
-        val additionalBuilds = try {
-            (start to info.lastBuild.number.toInt) map { number =>
-              Build(number.toString, jenkinsUrl +"/job/"+ job.name +"/"+ number +"/")
-            }
-          } catch {
-            case x: Exception =>
-              List[Build]()
-          }
-        (reportedBuilds ++ additionalBuilds)
-      }
 
-    allBuilds.reverse.toStream.map(build => buildStatus(job, build.number))
+    val allBuilds = {
+      val additionalBuilds = try {
+        (start to info.lastBuild.number.toInt) map { number =>
+          Build(number.toString, jenkinsUrl + "/job/" + job.name + "/" + number + "/")
+        }
+      } catch {
+        case x: Exception =>
+          List[Build]()
+      }
+      (reportedBuilds ++ additionalBuilds)
+    }
+
+    // queued items must come first, they have been added more recently or they wouldn't have been queued
+    queuedStati.toStream ++ allBuilds.reverse.toStream.map(build => buildStatus(job, build.number))
   }
-    // new Traversable[BuildStatus]{
-    //   override def foreach[U](f: BuildStatus => U): Unit = {
-    //     val info = jobInfo(job)
-    //     // println("buildStatusForJob: "+ job.name +" -> "+ info)
-    //     for {
-    //       build <- info.builds.sorted.reverse
-    //       status = buildStatus(job, build.number)
-    //     } try f(status) catch {
-    //       case t: Exception => // Ignore this one.
-    //     }
-    //   }
-    // }
 }
 
 object API {
-  def apply(url: String, auth: Option[(String,String)] = None) = new API(url, auth)
+  def apply(url: String, auth: Option[(String, String)] = None) = new API(url, auth)
 }
 
 case class Job(name: String,
-               description: String,
-               nextBuildNumber: String,
-               builds: List[Build],
-               queueItem: Option[QueueItem],
-               lastBuild: Build,
-               firstBuild: Build)
-   
+  description: String,
+  nextBuildNumber: String,
+  builds: List[Build],
+  queueItem: Option[QueueItem],
+  lastBuild: Build,
+  firstBuild: Build)
+
 case class Build(number: String, url: String) extends Ordered[Build] {
   def num: Int = number.toInt
   def compare(that: Build) = this.num - that.num
@@ -102,22 +92,31 @@ case class Param(name: String, value: String) {
   override def toString = "%s -> %s" format (name, value)
 }
 case class Actions(parameters: List[Param]) {
-  override def toString = "Parameters(%s)" format(parameters mkString ", ")
+  override def toString = "Parameters(%s)" format (parameters mkString ", ")
 }
-case class BuildStatus(number: String, 
-    result: String, 
-    building: Boolean, 
-    id: String,
-    timestamp: String,
-    duration: String,
-    actions: Actions,
-    url: String
-) {
+case class BuildStatus(number: String,
+  result: String,
+  building: Boolean,
+  id: String,
+  timestamp: String,
+  duration: String,
+  actions: Actions,
+  url: String) {
+
+  assert(!(building && queued), "Cannot both be building and queued.")
+
+  def queued = false
   def isSuccess = result == "SUCCESS"
-    
+
   // TODO - Is this ok to assume?  
   def timestampDate =
     new java.util.Date(timestamp.toLong)
 }
 
-case class QueueItem(buildable: Boolean)
+case class Queue(items: List[QueueItem])
+case class QueueItem(actions: Actions, task: Task, id: String) {
+  def jobName = task.name
+  // the url is fake but needs to be unique
+  def toStatus = new BuildStatus("0", "Queued build for " + task.name + " id: " + id, false, "0", "0", "0", actions, task.url + "/queued/" + id) { override def queued = true }
+}
+case class Task(name: String, url: String)
